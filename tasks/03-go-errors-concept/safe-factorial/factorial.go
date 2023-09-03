4cc148bc5eae3d6327a0137943e580eba9f8b0cb
user <ekhvalov@gmail.com>
2023-09-03 18:23:30 +0900

    Task 3.5.3 "Safe factorial" completed.

--- tasks/03-go-errors-concept/safe-factorial/factorial.go
+++ tasks/03-go-errors-concept/safe-factorial/factorial.go
@@ -1,17 +1,34 @@
 package factorial
 
+import (
+	"errors"
+	"fmt"
+)
+
 const maxDepth = 256
 
 // Реализуй нас.
 var (
-	ErrNegativeN error
-	ErrTooDeep   error
+	ErrNegativeN = errors.New("negative number")
+	ErrTooDeep   = errors.New("call stack is too deep")
 )
 
 // Calculate рекурсивно считает факториал входного числа n.
 // Если число меньше нуля, то возвращается ошибка ErrNegativeN.
 // Если для вычисления факториала потребуется больше maxDepth фреймов, то Calculate вернёт ErrTooDeep.
 func Calculate(n int) (int, error) {
-	// Реализуй меня.
-	return 0, nil
+	if n < 0 {
+		return 0, fmt.Errorf("%w: %d", ErrNegativeN, n)
+	}
+	if n > maxDepth {
+		return 0, fmt.Errorf("%w: %d", ErrTooDeep, maxDepth)
+	}
+	if n < 2 {
+		return 1, nil
+	}
+	m, err := Calculate(n - 1)
+	if err != nil {
+		return 0, err
+	}
+	return n * m, nil
 }
