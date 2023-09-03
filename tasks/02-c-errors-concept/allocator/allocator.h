9eae4b7cddd9c6b6333d20f4b37b05635918dd8c
user <ekhvalov@gmail.com>
2023-09-03 16:22:38 +0900

    Task 2.2 completed.

--- tasks/02-c-errors-concept/allocator/allocator.h
+++ tasks/02-c-errors-concept/allocator/allocator.h
@@ -7,5 +7,18 @@ extern int errno;
 
 void *allocate(int user_id, size_t size)
 {
-    // Реализуй меня.
+    if (user_id != ADMIN) {
+        errno = EPERM;
+        return NULL;
+    }
+    if (size < MIN_MEMORY_BLOCK) {
+        errno = EDOM;
+        return NULL;
+    }
+    void *m = malloc(size);
+    if (m == NULL) {
+        errno = ENOMEM;
+        return NULL;
+    }
+    return m;
 }
