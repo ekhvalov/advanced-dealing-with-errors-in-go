21a747d588aedd4ca5f5176908ca10ee873e1acd
user <ekhvalov@gmail.com>
2023-09-03 16:49:50 +0900

    Task 3.3.1 completed.

--- tasks/03-go-errors-concept/byte-reader-byte-writer/byte_buffer.go
+++ tasks/03-go-errors-concept/byte-reader-byte-writer/byte_buffer.go
@@ -1,16 +1,42 @@
 package bytebuffer
 
+import "fmt"
+
 const bufferMaxSize = 1024
 
 type MaxSizeExceededError struct {
 	desiredLen int
 }
 
+func (e *MaxSizeExceededError) Error() string {
+	return fmt.Sprintf("buffer max size exceeded: %d > %d", e.desiredLen, bufferMaxSize)
+}
+
 type EndOfBufferError struct{}
+
+func (e *EndOfBufferError) Error() string {
+	return "end of buffer"
+}
 
 type ByteBuffer struct {
 	buffer []byte
 	offset int
+}
+
+func (b *ByteBuffer) ReadByte() (byte, error) {
+	if b.offset >= len(b.buffer) {
+		return 0, new(EndOfBufferError)
+	}
+	b.offset++
+	return b.buffer[b.offset-1], nil
+}
+
+func (b *ByteBuffer) WriteByte(c byte) error {
+	if len(b.buffer)+1 > bufferMaxSize {
+		return &MaxSizeExceededError{desiredLen: len(b.buffer) + 1}
+	}
+	b.buffer = append(b.buffer, c)
+	return nil
 }
 
 // Необходимо сделать так, чтобы тип *ByteBuffer реализовывал интерфейсы io.ByteWriter и io.ByteReader.
