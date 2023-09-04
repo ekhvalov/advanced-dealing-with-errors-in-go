db8e6b11afb115d04345e00636810d242288e330
user <ekhvalov@gmail.com>
2023-09-04 22:29:04 +0900

    Task 3.8.3 "With time error" completed.

--- tasks/03-go-errors-concept/with-time-error/with_time_error.go
+++ tasks/03-go-errors-concept/with-time-error/with_time_error.go
@@ -1,10 +1,25 @@
 package errs
 
 import (
+	"fmt"
 	"time"
 )
 
 type WithTimeError struct { // Реализуй меня.
+	err error
+	t   time.Time
+}
+
+func (e *WithTimeError) Error() string {
+	return fmt.Sprintf("%v, occurred at: %s", e.err, e.t)
+}
+
+func (e *WithTimeError) Time() time.Time {
+	return e.t
+}
+
+func (e *WithTimeError) Unwrap() error {
+	return e.err
 }
 
 func NewWithTimeError(err error) error {
@@ -12,6 +27,5 @@ func NewWithTimeError(err error) error {
 }
 
 func newWithTimeError(err error, timeFunc func() time.Time) error {
-	// Реализуй меня.
-	return nil
+	return &WithTimeError{err: err, t: timeFunc()}
 }
