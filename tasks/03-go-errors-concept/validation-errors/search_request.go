3ebf37df68a1ad47d392a8c901a8237e0b86e740
user <ekhvalov@gmail.com>
2023-09-04 23:11:22 +0900

    Task 3.8.4 "Validation errors" completed.

--- tasks/03-go-errors-concept/validation-errors/search_request.go
+++ tasks/03-go-errors-concept/validation-errors/search_request.go
@@ -1,24 +1,41 @@
 package requests
 
 import (
-	// Доступные пакеты, _ для сохранения импортов.
-	_ "errors"
-	_ "fmt"
-	_ "regexp"
-	_ "strings"
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
 )
 
 const maxPageSize = 100
 
 // Реализуй нас.
 var (
-	errIsNotRegexp     error
-	errInvalidPage     error
-	errInvalidPageSize error
+	errIsNotRegexp     = errors.New("exp is not regexp")
+	errInvalidPage     = errors.New("invalid page")
+	errInvalidPageSize = errors.New("invalid page size")
 )
 
 // Реализуй мои методы.
 type ValidationErrors []error
+
+func (ve *ValidationErrors) Error() string {
+	sb := strings.Builder{}
+	sb.WriteString("validation errors:\n")
+	for _, err := range *ve {
+		sb.WriteString(fmt.Sprintf("\t%v\n", err))
+	}
+	return sb.String()
+}
+
+func (ve *ValidationErrors) Is(err error) bool {
+	for _, e := range *ve {
+		if errors.Is(e, err) {
+			return true
+		}
+	}
+	return false
+}
 
 type SearchRequest struct {
 	Exp      string
@@ -27,6 +44,22 @@ type SearchRequest struct {
 }
 
 func (r SearchRequest) Validate() error {
-	// Реализуй меня.
-	return nil
+	var vErrors ValidationErrors
+	_, err := regexp.Compile(r.Exp)
+	if err != nil {
+		vErrors = append(vErrors, fmt.Errorf("%w: %v", errIsNotRegexp, err))
+	}
+	if r.Page <= 0 {
+		vErrors = append(vErrors, fmt.Errorf("%w: %d", errInvalidPage, r.Page))
+	}
+	if r.PageSize <= 0 {
+		vErrors = append(vErrors, fmt.Errorf("%w: %d <= 0", errInvalidPageSize, r.PageSize))
+	}
+	if r.PageSize > maxPageSize {
+		vErrors = append(vErrors, fmt.Errorf("%w: %d > %d", errInvalidPageSize, r.PageSize, maxPageSize))
+	}
+	if len(vErrors) == 0 {
+		return nil
+	}
+	return &vErrors
 }
