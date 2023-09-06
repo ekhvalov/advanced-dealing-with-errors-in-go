1f2dabb2d112dc121644562b3708fbb2b2411d79
user <ekhvalov@gmail.com>
2023-09-06 21:17:43 +0900

    Task 3.9 "Extract" completed.

--- tasks/03-go-errors-concept/extract/extract.go
+++ tasks/03-go-errors-concept/extract/extract.go
@@ -3,6 +3,23 @@ package errs
 // Extract достаёт из цепочки err набор sentinel-ошибок,
 // игнорируя "оборачивающие" их ошибки.
 func Extract(err error) []error {
-	// Реализуй меня.
-	return nil
+	errs := make([]error, 0)
+	switch x := err.(type) {
+	case interface{ Unwrap() error }:
+		e := Extract(x.Unwrap())
+		if e != nil {
+			errs = append(errs, e...)
+		}
+	case interface{ Unwrap() []error }:
+		for _, e := range x.Unwrap() {
+			er := Extract(e)
+			if er != nil {
+				errs = append(errs, er...)
+			}
+		}
+	case nil:
+	default:
+		errs = append(errs, err)
+	}
+	return errs
 }
