37a65cb6389c961ed190a5ad853cf1b9c0d12921
user <ekhvalov@gmail.com>
2023-09-09 18:29:27 +0900

    Task 4.3.1 "Wrap nil" completed.

--- tasks/04-non-standard-modules/wrap-nil/errors.go
+++ tasks/04-non-standard-modules/wrap-nil/errors.go
@@ -1,6 +1,11 @@
 package errors
 
+import "fmt"
+
 // Wrapf работает аналогично fmt.Errorf, только поддерживает nil-ошибки.
 func Wrapf(err error, f string, v ...any) error {
-	return nil
+	if nil == err {
+		return nil
+	}
+	return fmt.Errorf("%s: %w", fmt.Sprintf(f, v...), err)
 }
