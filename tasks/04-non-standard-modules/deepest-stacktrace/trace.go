a7680d0ceb4a578135ded7c69b50b0a777c6bb50
user <ekhvalov@gmail.com>
2023-09-09 22:02:42 +0900

    Task 4.4.2 "Deepest stack trace" completed.

--- tasks/04-non-standard-modules/deepest-stacktrace/trace.go
+++ tasks/04-non-standard-modules/deepest-stacktrace/trace.go
@@ -1,11 +1,18 @@
 package trace
 
 import (
+	"errors"
 	"github.com/getsentry/sentry-go"
 )
 
 // GetDeepestStackTrace достаёт самый глубокий стектрейс из цепочки ошибок.
 func GetDeepestStackTrace(err error) *sentry.Stacktrace {
-	// Реализуй меня.
-	return nil
+	var stack *sentry.Stacktrace
+	for err != nil {
+		if st := sentry.ExtractStacktrace(err); st != nil {
+			stack = st
+		}
+		err = errors.Unwrap(err)
+	}
+	return stack
 }
