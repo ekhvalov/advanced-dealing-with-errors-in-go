d71a8ac1fdb4b3c092877b4fbe60dccc7e8afd36
user <ekhvalov@gmail.com>
2023-09-09 21:36:39 +0900

    Task 4.4.1 "Trim stack trace" completed.

--- tasks/04-non-standard-modules/trim-stacktrace/errors.go
+++ tasks/04-non-standard-modules/trim-stacktrace/errors.go
@@ -2,6 +2,20 @@ package errors
 
 // TrimStackTrace режет все стектрейсы в цепочке ошибок err.
 func TrimStackTrace(err error) error {
-	// Реализуй меня.
-	return err
+	if nil == err {
+		return nil
+	}
+	return &wrapper{err: err}
+}
+
+type wrapper struct {
+	err error
+}
+
+func (w *wrapper) Error() string {
+	return w.err.Error()
+}
+
+func (w *wrapper) Unwrap() error {
+	return w.err
 }
