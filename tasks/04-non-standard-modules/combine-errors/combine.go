d5f9d1736252a7f93ab81f141f536bd35992ff4f
user <ekhvalov@gmail.com>
2023-09-10 21:32:56 +0900

    Task 4.6 "Combine errors" completed.

--- tasks/04-non-standard-modules/combine-errors/combine.go
+++ tasks/04-non-standard-modules/combine-errors/combine.go
@@ -1,9 +1,54 @@
 package errors
 
+import (
+	"fmt"
+)
+
 // Combine "прицепляет" ошибки other к err так, что они начинают фигурировать при выводе
 // её на экран через спецификатор `%+v`. Если err является nil, то первостепенной ошибкой
 // становится первая из ошибок other.
 func Combine(err error, other ...error) error {
-	// Реализуй меня.
-	return nil
+	var i int
+	for ; err == nil && i < len(other); i++ {
+		if other[i] != nil {
+			err = other[i]
+		}
+	}
+	if nil == err {
+		return nil
+	}
+	ce := &combinedError{main: err}
+	ce.extra = make([]error, 0, len(other))
+	for ; i < len(other); i++ {
+		if other[i] != nil {
+			ce.extra = append(ce.extra, other[i])
+		}
+	}
+	return ce
+}
+
+type combinedError struct {
+	main  error
+	extra []error
+}
+
+func (ce *combinedError) Unwrap() error {
+	return ce.main
+}
+
+func (ce *combinedError) Error() string {
+	return ce.main.Error()
+}
+
+func (ce *combinedError) Format(state fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		_, _ = fmt.Fprint(state, ce.main.Error())
+		if state.Flag('+') && len(ce.extra) > 0 {
+			fmt.Fprintln(state)
+			for _, err := range ce.extra {
+				fmt.Fprintf(state, "  - %s\n", err.Error())
+			}
+		}
+	}
 }
