13e52fcf815e5c0ab72472b47ad45ea2300c0b54
user <ekhvalov@gmail.com>
2023-09-13 21:37:27 +0900

    Task 5.3.2 "HTTPError" completed.

--- tasks/05-errors-best-practices/http-error/http_error.go
+++ tasks/05-errors-best-practices/http-error/http_error.go
@@ -1,13 +1,26 @@
 package httperr
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Реализуй нас.
 var (
-	ErrStatusOK                  error
-	ErrStatusBadRequest          error
-	ErrStatusNotFound            error
-	ErrStatusUnprocessableEntity error
-	ErrStatusInternalServerError error
+	ErrStatusOK                  error = HTTPError(http.StatusOK)
+	ErrStatusBadRequest          error = HTTPError(http.StatusBadRequest)
+	ErrStatusNotFound            error = HTTPError(http.StatusNotFound)
+	ErrStatusUnprocessableEntity error = HTTPError(http.StatusUnprocessableEntity)
+	ErrStatusInternalServerError error = HTTPError(http.StatusInternalServerError)
 )
 
 // Реализуй меня.
 type HTTPError int
+
+func (e HTTPError) Error() string {
+	return fmt.Sprintf("%d %s", e.Code(), http.StatusText(e.Code()))
+}
+
+func (e HTTPError) Code() int {
+	return int(e)
+}
