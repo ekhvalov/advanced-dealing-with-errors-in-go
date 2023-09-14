3a5f8b71d3f9dee0716525a5e720e0313ce3182a
user <ekhvalov@gmail.com>
2023-09-14 21:08:09 +0900

    Task 5.5 "ParseAndExecuteTemplate" completed.

--- tasks/05-errors-best-practices/template-err/tmpl.go
+++ tasks/05-errors-best-practices/template-err/tmpl.go
@@ -1,16 +1,25 @@
 package tmpl
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"io"
 )
 
 var (
-	errParseTemplate   error
-	errExecuteTemplate error
+	errParseTemplate   = errors.New("parse error")
+	errExecuteTemplate = errors.New("execute error")
 )
 
-func ParseAndExecuteTemplate(wr io.Writer, name, text string, data any) {
-	t, _ := template.New(name).Parse(text)
-	t.Execute(wr, data)
+func ParseAndExecuteTemplate(wr io.Writer, name, text string, data any) error {
+	t, err := template.New(name).Parse(text)
+	if err != nil {
+		return fmt.Errorf("%w: %v", errParseTemplate, err.Error())
+	}
+	err = t.Execute(wr, data)
+	if err != nil {
+		return fmt.Errorf("%w: %v", errExecuteTemplate, err.Error())
+	}
+	return nil
 }
