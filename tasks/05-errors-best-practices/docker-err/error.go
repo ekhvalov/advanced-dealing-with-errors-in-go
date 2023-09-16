4ea8787be7cecbacf2271f3114e74da65ed84bb5
user <ekhvalov@gmail.com>
2023-09-16 09:53:33 +0900

    Task 5.6.2 "Docker error" completed.

--- tasks/05-errors-best-practices/docker-err/error.go
+++ tasks/05-errors-best-practices/docker-err/error.go
@@ -1,11 +1,69 @@
 package docker
 
-type Error struct{}
+import (
+	"regexp"
+	"strings"
+)
 
-func (e *Error) Error() string {
-	panic("implement me")
+type errorCode int
+
+const (
+	errorCodeUnknown errorCode = iota
+	errorCodePullAccessDenied
+	errorCodeNoSuchContainer
+	errorCodeContainerNotRunning
+)
+
+const (
+	pullAccessDeniedMsg = "pull access denied"
+	noSuchContainerMsg  = "No such container"
+)
+
+var containerNotRunningRe = regexp.MustCompile(`Container \w+ is not running`)
+
+var _ interface {
+	error
+	IsPullAccessDeniedError() bool
+	IsNoSuchContainerError() bool
+	IsContainerNotRunningError() bool
+} = (*Error)(nil)
+
+type Error struct {
+	err  error
+	code errorCode
 }
 
 func newDockerError(err error) *Error {
-	return &Error{}
+	var errCode errorCode
+	switch {
+	case strings.Contains(err.Error(), pullAccessDeniedMsg):
+		errCode = errorCodePullAccessDenied
+	case strings.Contains(err.Error(), noSuchContainerMsg):
+		errCode = errorCodeNoSuchContainer
+	case containerNotRunningRe.MatchString(err.Error()):
+		errCode = errorCodeContainerNotRunning
+	default:
+		errCode = errorCodeUnknown
+	}
+	return &Error{err: err, code: errCode}
+}
+
+func (e *Error) Unwrap() error {
+	return e.err
+}
+
+func (e *Error) IsPullAccessDeniedError() bool {
+	return e.code == errorCodePullAccessDenied
+}
+
+func (e *Error) IsNoSuchContainerError() bool {
+	return e.code == errorCodeNoSuchContainer
+}
+
+func (e *Error) IsContainerNotRunningError() bool {
+	return e.code == errorCodeContainerNotRunning
+}
+
+func (e *Error) Error() string {
+	return e.err.Error()
 }
