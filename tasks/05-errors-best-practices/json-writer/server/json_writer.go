16a1622f274d3a508fd2d96c54ae3258c838a106
user <ekhvalov@gmail.com>
2023-09-24 23:25:45 +0900

    Task 5.8.2 "JSON writer" completed.

--- tasks/05-errors-best-practices/json-writer/server/json_writer.go
+++ tasks/05-errors-best-practices/json-writer/server/json_writer.go
@@ -1,8 +1,29 @@
 package server
 
-// Реализуй меня.
-type jsonWriter struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type jsonWriter struct {
+	log ILogger
+	w   http.ResponseWriter
+}
 
 func (jw jsonWriter) Write(v any) {
-	// Реализуй меня.
+	content, err := json.Marshal(v)
+	statusCode := http.StatusOK
+	if nil == err {
+		jw.w.Header().Set("Content-Type", "application/json")
+	} else {
+		jw.log.Error(fmt.Sprintf("JSON marshal error: %v", err))
+		statusCode = http.StatusInternalServerError
+		content = []byte(err.Error())
+	}
+	jw.w.WriteHeader(statusCode)
+	_, writeErr := jw.w.Write(content)
+	if writeErr != nil {
+		jw.log.Error(fmt.Sprintf("response write error: %v", err))
+	}
 }
