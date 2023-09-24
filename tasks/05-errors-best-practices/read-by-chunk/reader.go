0e1c6fa0bf51e08c44bab067640c1a701f060d34
user <ekhvalov@gmail.com>
2023-09-24 19:50:49 +0900

    Task 5.8.1 "Read by chunk" completed.

--- tasks/05-errors-best-practices/read-by-chunk/reader.go
+++ tasks/05-errors-best-practices/read-by-chunk/reader.go
@@ -1,9 +1,38 @@
 package reader
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 var ErrInvalidChunkSize error
 
 func ReadByChunk(r io.Reader, chunkSize int) ([][]byte, error) {
-	return nil, nil
+	if r == nil {
+		return nil, nil
+	}
+
+	if chunkSize <= 0 {
+		return nil, fmt.Errorf("%w: %v", ErrInvalidChunkSize, chunkSize)
+	}
+
+	var result [][]byte
+
+	for {
+		chunk := make([]byte, chunkSize)
+		n, err := io.ReadFull(r, chunk)
+
+		if err == io.EOF {
+			break
+		}
+		if err == io.ErrUnexpectedEOF {
+			chunk = chunk[:n]
+		} else if err != nil {
+			return nil, err
+		}
+
+		result = append(result, chunk)
+	}
+
+	return result, nil
 }
