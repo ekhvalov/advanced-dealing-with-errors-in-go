ef269c5d370a053b8b1ac5925135bfffedb54f63
user <ekhvalov@gmail.com>
2023-09-26 21:37:39 +0900

    Task 5.11 "Error with context" completed.

--- tasks/05-errors-best-practices/error-context/context.go
+++ tasks/05-errors-best-practices/error-context/context.go
@@ -1,13 +1,42 @@
 package errctx
 
+import "errors"
+
 type Fields map[string]any
 
 func AppendTo(err error, fields Fields) error {
-	// Реализуй меня.
-	return nil
+	if nil == err {
+		return nil
+	}
+	fe := &fieldsError{err: err, fields: make(Fields, len(fields))}
+	for k, v := range fields {
+		fe.fields[k] = v
+	}
+	return fe
 }
 
 func From(err error) Fields {
-	// Реализуй меня.
-	return nil
+	fields := make(Fields)
+	for err != nil {
+		if errFields, ok := err.(*fieldsError); ok {
+			for k, v := range errFields.fields {
+				fields[k] = v
+			}
+		}
+		err = errors.Unwrap(err)
+	}
+	return fields
+}
+
+type fieldsError struct {
+	err    error
+	fields Fields
+}
+
+func (e *fieldsError) Error() string {
+	return e.err.Error()
+}
+
+func (e *fieldsError) Unwrap() error {
+	return e.err
 }
