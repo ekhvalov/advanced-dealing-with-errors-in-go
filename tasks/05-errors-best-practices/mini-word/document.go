c055ecdea0b37e716de402ca954c416c8573540c
user <ekhvalov@gmail.com>
2023-09-27 21:19:00 +0900

    Task 5.12.2 "Mini-word" completed.

--- tasks/05-errors-best-practices/mini-word/document.go
+++ tasks/05-errors-best-practices/mini-word/document.go
@@ -1,38 +1,78 @@
 package miniword
 
 import (
+	"errors"
+	"fmt"
 	"io"
 )
 
 const maxPages = 3
 
 var (
-	errInvalidPageNumber error
-	errNoMorePages       error
-	errEmptyText         error
+	errInvalidPageNumber = errors.New("invalid page number")
+	errNoMorePages       = errors.New("no more pages")
+	errEmptyText         = errors.New("empty text")
 )
 
-type Document struct { // Реализуй меня.
+type Document struct {
+	pageNum int
+	pages   []string
+	err     error
 }
 
 func NewDocument() *Document {
-	// Реализуй меня.
-	return nil
+	return &Document{
+		pageNum: 0,
+		pages:   make([]string, 1, maxPages),
+	}
 }
 
 func (d *Document) AddPage() {
-	// Реализуй меня.
+	if d.err != nil {
+		return
+	}
+	if len(d.pages) >= maxPages {
+		d.err = errNoMorePages
+		return
+	}
+	d.pages = append(d.pages, "")
 }
 
 func (d *Document) SetActivePage(number int) {
-	// Реализуй меня.
+	if d.err != nil {
+		return
+	}
+	if number < 1 || number > maxPages {
+		d.err = errInvalidPageNumber
+		return
+	}
+	d.pageNum = number - 1
 }
 
 func (d *Document) WriteText(s string) {
-	// Реализуй меня.
+	if d.err != nil {
+		return
+	}
+	if len(s) == 0 {
+		d.err = errEmptyText
+	}
+	d.pages[d.pageNum] = s
 }
 
-func (d *Document) WriteTo(w io.Writer) (n int64, err error) {
-	// Реализуй меня.
-	return 0, nil
+func (d *Document) WriteTo(w io.Writer) (int64, error) {
+	if d.err != nil {
+		return 0, d.err
+	}
+	n := 0
+	for num, page := range d.pages {
+		if len(page) > 0 {
+			nn, err := fmt.Fprintf(w, "--- Page %d ---\n%s\n", num+1, page)
+			n += nn
+			if err != nil {
+				return int64(n), err
+			}
+		}
+
+	}
+	return int64(n), nil
 }
