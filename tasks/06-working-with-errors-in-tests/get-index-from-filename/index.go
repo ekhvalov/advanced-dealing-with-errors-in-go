72e8e0baf56d068ba4c8a918522a9ef8cb3a274c
user <ekhvalov@gmail.com>
2023-09-28 22:40:50 +0900

    Task 6.3 "Get index from filename" completed.

--- tasks/06-working-with-errors-in-tests/get-index-from-filename/index.go
+++ tasks/06-working-with-errors-in-tests/get-index-from-filename/index.go
@@ -3,6 +3,7 @@ package index
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -12,17 +13,40 @@ const prefix = "parsed_page_" // parsed_page_100
 var (
 	ErrInvalidFilename     = errors.New("invalid filename")
 	ErrIndexMustBePositive = errors.New("index must be > 0")
+	filenameRegex          = regexp.MustCompile(`^parsed_page_(\d+)$`)
 )
 
 func GetIndexFromFileName(fileName string) (int, error) {
+	// return getIndexFromFileNameSplit(fileName)
+	return getIndexFromFileNameRegex(fileName)
+}
+
+func getIndexFromFileNameRegex(fileName string) (int, error) {
+	result := filenameRegex.FindStringSubmatch(fileName)
+	if len(result) < 2 {
+		return 0, fmt.Errorf("%w: no index in filename %q", ErrInvalidFilename, fileName)
+	}
+	index, err := strconv.ParseInt(result[1], 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("cannot parse index as int: %w", err)
+	}
+
+	if index <= 0 {
+		return 0, fmt.Errorf("%w: got %d", ErrIndexMustBePositive, index)
+	}
+
+	return int(index), nil
+}
+
+func getIndexFromFileNameSplit(fileName string) (int, error) {
 	parts := strings.Split(fileName, prefix)
 	if len(parts) != 2 || parts[1] == "" {
 		return 0, fmt.Errorf("%w: no index in filename %q", ErrInvalidFilename, fileName)
 	}
 
-	num := parts[1]
+	// num := parts[1]
 
-	index, err := strconv.ParseInt(num, 10, 32)
+	index, err := strconv.ParseInt(parts[1], 10, 32)
 	if err != nil {
 		return 0, fmt.Errorf("cannot parse index as int: %w", err)
 	}
